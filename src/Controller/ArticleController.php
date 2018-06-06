b049c2c3ca734407ec9a290dc2acc6f4e5a77883
Benjamin Hughes <BHughes@Markettiers4dc.com>
2018-06-06 11:41:58 +0100

    Second Commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,11 +5,29 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+    /**
+     * @Route() ("/")
+     */
+
     public function homepage()
     {
         return new Response("Hello World");
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+
+
+    public function show($slug){
+        return new Response(sprintf(
+            "Future page to show article: %s",
+            $slug
+        ));
+
     }
 }
