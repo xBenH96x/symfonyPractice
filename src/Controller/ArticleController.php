9bda6e9deb01fc25dc9fe07fd6fbf7da494ad1dc
Benjamin Hughes <BHughes@Markettiers4dc.com>
2018-06-06 12:35:21 +0100

    Lost count

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route() ("/")
@@ -24,10 +25,20 @@ class ArticleController
 
 
     public function show($slug){
-        return new Response(sprintf(
-            "Future page to show article: %s",
-            $slug
-        ));
+
+
+        $comments = [
+            'I ate something',
+            'I like pears, man',
+            'I love TURTLES',
+            ];
+
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-',' ', $slug)),
+            'comments'=> $comments
+            ]);
+
 
     }
 }
