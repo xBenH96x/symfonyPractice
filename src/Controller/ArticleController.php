ae93aa119e2cb1834a50413e8dc75eb0a01a982c
Benjamin Hughes <BHughes@Markettiers4dc.com>
2018-06-06 14:01:52 +0100

    x commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -34,6 +34,8 @@ class ArticleController extends AbstractController
             ];
 
 
+        dump($slug, $this);
+
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-',' ', $slug)),
             'comments'=> $comments
